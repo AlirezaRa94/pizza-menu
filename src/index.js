a3bd898122ef3b4e031b5500143453fca98d9f5d
Alireza Raei <alirezaraei17@gmail.com>
2023-11-17 18:17:24 +1100

    feat: Integrate pizza menu and footer in App component
    - Add Header, Pizza, Menu, and Footer components
    - Display pizza information from data source
    - Create footer with current time display
    - Update App component to include Header, Menu, and Footer

--- src/index.js
+++ src/index.js
@@ -1,8 +1,44 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { pizzaData } from "./data";
+
+function Header() {
+  return <h1>Pizza Hot!</h1>;
+}
+
+function Pizza() {
+  return (
+    <div>
+      <img src={pizzaData[0].photoName} alt={pizzaData[0].name} />
+      <h3>{pizzaData[0].name}</h3>
+    </div>
+  );
+}
+
+function Menu() {
+  return (
+    <div>
+      <h2>Our Menu</h2>
+      <Pizza />
+      <Pizza />
+    </div>
+  );
+}
+
+function Footer() {
+  return (
+    <footer>{new Date().toLocaleTimeString()}. We are currently open!</footer>
+  );
+}
 
 function App() {
-  return <h1>Hello React!</h1>;
+  return (
+    <div>
+      <Header />
+      <Menu />
+      <Footer />
+    </div>
+  );
 }
 
 // React v18
@@ -14,4 +50,4 @@ root.render(
 );
 
 // React before 18
-ReactDOM.render(<App />, document.getElementById("root"));
+// ReactDOM.render(<App />, document.getElementById("root"));
